a5ce5c1169f5029264afec9eb98f04c3482a8894
Vladimiro Paschali <vladimiropaschali@gmail.com>
2024-01-21 18:13:51 +0100

    Metamask

--- website/app.py
+++ website/app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # return render_template('index.html')
+    return render_template('metamask.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
