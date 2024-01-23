c72e938f4cb7a8b7bc9030297570e81158d62c5d
Raffaele Leo <raffaeleleo@Raffaeles-MacBook-Pro.local>
2024-01-23 15:57:51 +0100

    added nft to website

--- scripts/mint-nfts.ts
+++ scripts/mint-nfts.ts
@@ -8,12 +8,15 @@ async function main() {
 
     console.log("NFT Contract deployed to:", nft.tokenURI);
 
-    const tokenURI = "https://gateway.pinata.cloud/ipfs/ipfs/Qmax9pXv3wTgSi4DsNa2msGuZL6nAuhm4bV1QBcKG47rGM";
+    const tokenURI = "https://gateway.pinata.cloud/ipfs/Qmax9pXv3wTgSi4DsNa2msGuZL6nAuhm4bV1QBcKG47rGM";
     const txn = await nft.mintNFT(owner.address, tokenURI);
     await txn.wait();
 
     console.log("chainId: ", txn.blockNumber)
     console.log("NFT minted:", txn.hash);
+
+    const a = await nft.getCurrentTokenId()
+    console.log(await nft.tokenURI(a))
 }
 
 main().catch((error) => {
