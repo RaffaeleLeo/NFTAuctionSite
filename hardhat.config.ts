27883eda29f1cc964df5770e46d6d2114c302903
Vladimiro Paschali <vladimiropaschali@gmail.com>
2024-02-01 16:56:40 +0100

    new chain id

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,12 @@ import "@nomiclabs/hardhat-web3";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  defaultNetwork:"hardhat",
+  networks:{
+    hardhat:{
+      chainId:1137
+    }
+  }
 };
 
 export default config;
